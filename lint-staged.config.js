0efb9502e822dbb67e60e51fc011b68142f1dc57
Ahmed Amr Mohamed <ahmedamr4f@gmail.com>
2025-05-03 02:46:23 +0300

    Navbar fixes

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  '**/*.{js,jsx,ts,tsx}': (filenames) => [`yarn eslint --fix ${filenames.join(' ')}`],
-  '**/*.ts?(x)': () => `yarn build:types`,
-  '**/*.(md|json)': (filenames) => `yarn prettier --write ${filenames.join(' ')}`,
+  '**/*.{js,jsx,ts,tsx}': [
+    'eslint --fix',
+    'git add'
+  ],
+  '**/*.{md,json}': [
+    'prettier --write',
+    'git add'
+  ]
 };
